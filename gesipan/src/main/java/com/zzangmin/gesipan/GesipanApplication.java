c23beca40581b9513b56f9f94c0cd20b06c0d971
leezzangmin <ckdals12345678@gmail.com>
2022-06-23 16:19:43 +0900

    project init

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/GesipanApplication.java
@@ -0,0 +1,13 @@
+package com.zzangmin.gesipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesipanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesipanApplication.class, args);
+	}
+
+}
---
+++ gesipan/src/test/java/com/zzangmin/gesipan/GesipanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzangmin.gesipan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
