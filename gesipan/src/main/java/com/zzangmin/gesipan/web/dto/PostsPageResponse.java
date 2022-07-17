bcb1780aa16a70d7bdc7dc2727c9db757738009f
leezzangmin <ckdals12345678@gmail.com>
2022-07-17 23:13:24 +0900

    feat: 페이징 DTO 생성

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/dto/PostsPageResponse.java
@@ -0,0 +1,52 @@
+package com.zzangmin.gesipan.web.dto;
+
+import com.zzangmin.gesipan.web.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class PostsPageResponse {
+    private Long categoryId;
+    private List<PostPageResponse> postPageResponseList;
+
+    @Getter
+    @Builder
+    private static class PostPageResponse {
+        private Long postId;
+        private String postSubject;
+        private LocalDateTime createdAt;
+        private Long hitCount;
+        private int recommendCount;
+
+        private Long userId;
+        private String userNickname;
+
+        public static PostPageResponse of(Post post) {
+            return PostPageResponse.builder()
+                    .postId(post.getPostId())
+                    .postSubject(post.getPostSubject())
+                    .createdAt(post.getCreatedAt())
+                    .hitCount(post.getHitCount())
+                    .userId(post.getUser().getId())
+                    .userNickname(post.getUser().getUserNickname())
+                    .build();
+        }
+    }
+
+    private PostsPageResponse(Long categoryId, List<PostPageResponse> postPageResponses) {
+        this.categoryId = categoryId;
+        this.postPageResponseList = postPageResponses;
+    }
+
+    public static PostsPageResponse of(Long categoryId, List<Post> posts) {
+        return new PostsPageResponse(categoryId, posts.stream()
+                .map(i -> PostPageResponse.of(i))
+                .collect(Collectors.toList()));
+    }
+
+
+}
