0b3ac73ffc460abcf45175cbc8b4957d898e949c
leezzangmin <ckdals12345678@gmail.com>
2022-07-20 22:06:27 +0900

    feat: 게시글 DTO 검증 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/web/dto/PostSaveRequest.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/dto/PostSaveRequest.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
@@ -17,15 +18,17 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class PostSaveRequest {
 
-    @Length(max = 1000) @NotNull
+    @NotNull
+    @Length(max = 1000)
     private final String postSubject;
     @NotNull
     private final String postContent;
     @NotNull @Positive
     private final Long userId;
-    @NotNull
+    @NotNull @Positive
     private final Long postCategoryId;
     @NotNull
+    @PastOrPresent
     private final LocalDateTime createdAt;
 
 }
