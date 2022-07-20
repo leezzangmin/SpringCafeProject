1f24c2337feccaaac78220d1b820594c1ad6c915
leezzangmin <ckdals12345678@gmail.com>
2022-07-20 22:07:50 +0900

    feat: Comment 등록 dto 생성 및 service 레이어 로직 작성

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/service/CommentService.java
@@ -0,0 +1,32 @@
+package com.zzangmin.gesipan.web.service;
+
+import com.zzangmin.gesipan.dao.CommentRepository;
+import com.zzangmin.gesipan.dao.PostRepository;
+import com.zzangmin.gesipan.dao.UserRepository;
+import com.zzangmin.gesipan.web.dto.CommentSaveRequest;
+import com.zzangmin.gesipan.web.entity.Comment;
+import com.zzangmin.gesipan.web.entity.Post;
+import com.zzangmin.gesipan.web.entity.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    public Long save(CommentSaveRequest commentSaveRequest) {
+        Users user = userRepository.findById(commentSaveRequest.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 없습니다. 잘못된 입력"));
+        Post post = postRepository.findById(commentSaveRequest.getReferencePostId())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 게시물이 없습니다. 잘못된 입력"));
+        Comment comment = commentSaveRequest.toEntity(user, post);
+        return commentRepository.save(comment).getCommentId();
+    }
+
+}
