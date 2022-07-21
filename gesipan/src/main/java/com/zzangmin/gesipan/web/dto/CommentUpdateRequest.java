840af745544e4c1d3cae4e7ed730e38657f98f99
leezzangmin <ckdals12345678@gmail.com>
2022-07-21 23:11:40 +0900

    feat: comment update dto 생성

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/dto/CommentUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.zzangmin.gesipan.web.dto;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentUpdateRequest {
+
+    @NotNull
+    private String commentContent;
+    @NotNull
+    @PastOrPresent
+    private LocalDateTime updatedAt;
+
+}
