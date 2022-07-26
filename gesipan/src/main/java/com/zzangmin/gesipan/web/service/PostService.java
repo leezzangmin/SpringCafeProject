25e307745501fd01a4e448c8e1fa95df23e08424
leezzangmin <ckdals12345678@gmail.com>
2022-07-26 13:46:19 +0900

    style: 불필요 import 제거

--- gesipan/src/main/java/com/zzangmin/gesipan/web/service/PostService.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/service/PostService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 
