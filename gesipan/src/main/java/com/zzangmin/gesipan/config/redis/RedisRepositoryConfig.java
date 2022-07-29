14ccec7fe9ae6834081f6acc6f77855404ee67a2
leezzangmin <ckdals12345678@gmail.com>
2022-07-29 22:19:44 +0900

    feat: 레디스 StringRedisSerializer 설정

--- gesipan/src/main/java/com/zzangmin/gesipan/config/redis/RedisRepositoryConfig.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/config/redis/RedisRepositoryConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
@@ -25,6 +26,8 @@ public class RedisRepositoryConfig {
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
