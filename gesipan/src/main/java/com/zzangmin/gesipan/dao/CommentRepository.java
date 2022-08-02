eea958207b34d5ee61f0ac782849887df863fe79
leezzangmin <ckdals12345678@gmail.com>
2022-08-02 22:17:30 +0900

    feat: postId 리스트로 comment 개수 리스트 조회하는 쿼리 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/dao/CommentRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/dao/CommentRepository.java
@@ -13,4 +13,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join fetch c.user where c.post.postId=:postId")
     List<Comment> findAllByPostId(@Param("postId") Long postId);
+
+    @Query("select count(c.post.postId) from Post p left join Comment c on p.postId = c.post.postId where p.postId in :postIds group by p.postId")
+    List<Integer> countByIds(@Param("postIds") List<Long> postIds);
 }
