72e79d199d1729a2a70800cd5fc840952eeee881
leezzangmin <ckdals12345678@gmail.com>
2022-08-03 00:14:33 +0900

    feat: 개인 댓글 가져오는 쿼리 작성

--- gesipan/src/main/java/com/zzangmin/gesipan/dao/CommentRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/dao/CommentRepository.java
@@ -16,4 +16,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select count(c.post.postId) from Post p left join Comment c on p.postId = c.post.postId where p.postId in :postIds group by p.postId")
     List<Integer> countByIds(@Param("postIds") List<Long> postIds);
+
+    @Query("select c from Comment c join fetch c.post where c.user.userId=:userId")
+    List<Comment> findAllByUserIdWithPostId(@Param("userId") Long userId);
 }
