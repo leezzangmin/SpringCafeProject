1d1ec6a1e4ca177d0f3151232aa05af09e6c4278
leezzangmin <ckdals12345678@gmail.com>
2022-08-08 19:22:51 +0900

    feat: users엔티티 @ToString 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/web/entity/Users.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/entity/Users.java
@@ -2,13 +2,11 @@ package com.zzangmin.gesipan.web.entity;
 
 import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@ToString
 @Getter
 @Builder
 @NoArgsConstructor
