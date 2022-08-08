d1e58bcff1cec086d913bda328a4222c6f9cef70
leezzangmin <ckdals12345678@gmail.com>
2022-08-08 18:53:45 +0900

    rename: UserRepository -> UsersRepository 이름변경

--- gesipan/src/main/java/com/zzangmin/gesipan/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zzangmin.gesipan.dao;
-
-import com.zzangmin.gesipan.web.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
