4452992696d49dda86133f1b97ad9ab8f17024d0
leezzangmin <ckdals12345678@gmail.com>
2022-08-09 17:31:29 +0900

    test: PostControllerTest 생성

---
+++ gesipan/src/test/java/com/zzangmin/gesipan/web/controller/PostControllerTest.java
@@ -0,0 +1,12 @@
+package com.zzangmin.gesipan.web.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void myPosts() {
+    }
+}
