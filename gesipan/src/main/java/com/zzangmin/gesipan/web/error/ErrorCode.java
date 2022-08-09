1a6903029e994c551c8b249a8da2e57005de9953
leezzangmin <ckdals12345678@gmail.com>
2022-08-09 16:31:17 +0900

    feat: MissingServletRequestParameterException 익셉션 처리 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/web/error/ErrorCode.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/error/ErrorCode.java
@@ -6,8 +6,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // Common
-    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "C002", " Invalid Method Value"),
+    INVALID_INPUT_VALUE(400, "C001", "Invalid Input Value"),
+    MISSING_INPUT_VALUE(400, "C003", "Missing Input Value"),
+    METHOD_NOT_ALLOWED(405, "C002", "Invalid Method Value"),
     HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
     INTERNAL_SERVER_ERROR(500, "C007", "Server Error"),
     ENUM_TYPE_MISSMATCH(400, "C008", "Enum type not matched"),
@@ -17,9 +18,7 @@ public enum ErrorCode {
     // Query
     INVALID_QUERY_ARGUMENT(404, "D001", "Data not found"),
     INVALID_QUERY_STATE(400, "D002", "Data state not valid"),
-
             ;
-
 
     private final String code;
     private final String message;
