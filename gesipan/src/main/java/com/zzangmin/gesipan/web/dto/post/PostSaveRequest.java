271444853c5d593b79b10d0bdf4465160d4ae0f0
leezzangmin <ckdals12345678@gmail.com>
2022-08-14 16:56:08 +0900

    feat: NotBlank 작동 안됨 NotNull로 변경

--- gesipan/src/main/java/com/zzangmin/gesipan/web/dto/post/PostSaveRequest.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/dto/post/PostSaveRequest.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
@@ -23,11 +24,12 @@ public class PostSaveRequest {
     private String postSubject;
     @NotBlank
     private String postContent;
-    @NotBlank @Positive
+    @NotNull
+    @Positive
     private Long userId;
-    @NotBlank @Positive
+    @NotNull @Positive
     private Long postCategoryId;
-    @NotBlank
+    @NotNull
     @PastOrPresent
     private LocalDateTime createdAt;
 
