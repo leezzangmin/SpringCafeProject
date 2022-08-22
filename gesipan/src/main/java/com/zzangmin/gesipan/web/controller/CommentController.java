2b245ebe5205354f21ab540d35b2e397337d63ce
leezzangmin <ckdals12345678@gmail.com>
2022-08-22 13:20:58 +0900

    feat: 로깅 추가 및 내 댓글 목록 jwt에서 정보 뽑아옴

--- gesipan/src/main/java/com/zzangmin/gesipan/web/controller/CommentController.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/controller/CommentController.java
@@ -3,21 +3,30 @@ package com.zzangmin.gesipan.web.controller;
 import com.zzangmin.gesipan.web.dto.comment.CommentSaveRequest;
 import com.zzangmin.gesipan.web.dto.comment.CommentUpdateRequest;
 import com.zzangmin.gesipan.web.dto.comment.PersonalCommentsResponse;
+import com.zzangmin.gesipan.web.entity.Users;
+import com.zzangmin.gesipan.web.jwt.JwtProvider;
 import com.zzangmin.gesipan.web.service.CommentService;
+import com.zzangmin.gesipan.web.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class CommentController {
 
     private final CommentService commentService;
+    private final JwtProvider jwtProvider;
+    private final UsersService usersService;
 
     @PostMapping("/comment")
     public ResponseEntity<Long> createComment(@RequestBody @Valid CommentSaveRequest commentSaveRequest) {
+        log.info("comment save: {}", commentSaveRequest);
         Long savedCommentId = commentService.save(commentSaveRequest);
         return ResponseEntity.ok(savedCommentId);
     }
@@ -34,11 +43,15 @@ public class CommentController {
         return ResponseEntity.ok("update success!");
     }
 
-    // TODO: requestparam 추후 user 연동시 RESTful하게 개선하기
     @GetMapping("/comments/my")
-    public ResponseEntity myComments(@RequestParam Long userId) {
-        PersonalCommentsResponse personalCommentsResponse = commentService.userComments(userId);
-        return ResponseEntity.ok(personalCommentsResponse);
+    public ResponseEntity<PersonalCommentsResponse> myComments(HttpServletRequest request) {
+        String jwt = jwtProvider.resolveToken(request).
+                orElseThrow(() -> new IllegalStateException("뭔가 잘못된 인증 요청"));
+        log.info("my comments jwt: {}", jwt);
+        String userInfo = jwtProvider.getUserInfo(jwt);
+        Users user = usersService.findOneByEmail(userInfo);
+
+        return ResponseEntity.ok(commentService.userComments(user.getUserId()));
     }
 
 }
