d3ab17227eb7965667858eb0c54a981b937d2340
leezzangmin <ckdals12345678@gmail.com>
2022-08-22 14:33:06 +0900

    feat: config 파일 통합
    - WebMvcConfigurationSupport->WebMvcConfigurer

--- gesipan/src/main/java/com/zzangmin/gesipan/config/WebConfig.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/config/WebConfig.java
@@ -1,19 +1,19 @@
-package com.zzangmin.gesipan.config.interceptor;
+package com.zzangmin.gesipan.config;
 
 import com.zzangmin.gesipan.web.interceptor.LoginCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
-public class InterceptorConfig extends WebMvcConfigurationSupport {
+public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login","/post/{postId}", "/posts", "/oauth/github_url")
