6e11ff95f1813d2e25f01b514c27a9ef59d8b656
leezzangmin <ckdals12345678@gmail.com>
2022-08-27 23:52:56 +0900

    feat: 커스텀 argumentresolver 등록

--- gesipan/src/main/java/com/zzangmin/gesipan/config/WebConfig.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/config/WebConfig.java
@@ -1,18 +1,23 @@
 package com.zzangmin.gesipan.config;
 
+import com.zzangmin.gesipan.web.argumentresolver.UserIdArgumentResolver;
 import com.zzangmin.gesipan.web.interceptor.LoginCheckInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
+    private final UserIdArgumentResolver userIdArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -20,6 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login","/post/{postId}", "/posts", "/oauth/github_url")
                 .order(1);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
     }
 
     @Bean
