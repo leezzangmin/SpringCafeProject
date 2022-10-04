429a10ae63b1eb05767457e418a7a7340ae4e100
leezzangmin <ckdals12345678@gmail.com>
2022-10-04 16:39:55 +0900

    feat: 알림 생성 서비스로직 작성
    - 트랜잭션 전파속성 netsted로 하고싶은데 hibernate에서 지원 안함

--- gesipan/src/main/java/com/zzangmin/gesipan/web/service/NotificationService.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/service/NotificationService.java
@@ -2,12 +2,15 @@ package com.zzangmin.gesipan.web.service;
 
 
 import com.zzangmin.gesipan.dao.NotificationRepository;
+import com.zzangmin.gesipan.web.dto.notification.NotificationCreateRequest;
 import com.zzangmin.gesipan.web.dto.notification.NotificationsResponse;
 import com.zzangmin.gesipan.web.entity.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -17,8 +20,22 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    @Transactional(readOnly = true)
     public NotificationsResponse findAll(Long userId) {
         List<Notification> notifications = notificationRepository.findAllByUserId(userId);
         return NotificationsResponse.of(notifications);
+    }
+
+    @Transactional
+    public long createNotification(NotificationCreateRequest request) {
+        Notification notification = Notification.builder()
+                .targetUser(request.getTargetUser())
+                .publishedUser(request.getPublishedUser())
+                .notificationType(request.getNotificationType())
+                .referencePost(request.getReferencePost())
+                .notificationMessage(request.getNotificationMessage())
+                .createdAt(LocalDateTime.now())
+                .build();
+        return notificationRepository.save(notification).getNotificationId();
     }
 }
