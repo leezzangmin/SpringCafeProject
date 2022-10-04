f364ec67713bc35712a4a7059351a1f26c3a5e2a
leezzangmin <ckdals12345678@gmail.com>
2022-10-04 16:58:58 +0900

    feat: 알림 확인 요청 컨트롤러 매핑

--- gesipan/src/main/java/com/zzangmin/gesipan/web/controller/NotificationController.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/web/controller/NotificationController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -21,5 +23,14 @@ public class NotificationController {
         return ResponseEntity.ok(notificationService.findAll(userId));
     }
 
+    @PostMapping("/notifications/checked")
+    public ResponseEntity checkAllNotifications(@Auth Long userId) {
+        notificationService.checkAll(userId);
+    }
+
+    @PostMapping("/notifications/{notificationId}/checked")
+    public ResponseEntity checkOneNotification(@Auth Long userId, @PathVariable Long notificationId) {
+        notificationService.checkOne(userId);
+    }
 }
 
