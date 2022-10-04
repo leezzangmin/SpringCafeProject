e93a2b2b8fc10913c0d9a4de965121e3f4219495
leezzangmin <ckdals12345678@gmail.com>
2022-10-04 17:18:21 +0900

    feat: 유저 알림 확인 한방 업데이트 쿼리 작성

--- gesipan/src/main/java/com/zzangmin/gesipan/dao/NotificationRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/dao/NotificationRepository.java
@@ -2,9 +2,11 @@ package com.zzangmin.gesipan.dao;
 
 import com.zzangmin.gesipan.web.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
@@ -12,4 +14,9 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     @Query("select n from Notification n where n.targetUser.userId=:userId")
     List<Notification> findAllByUserId(@Param("userId") Long userId);
 
+    @Modifying
+    @Query("update Notification n set n.checkedAt =:now " +
+            "where n.notificationId in " +
+            "(select inn.notificationId from Notification inn where inn.checkedAt is not null and inn.targetUser.userId=:userId)")
+    List<Long> checkByUserId(Long userId, LocalDateTime now);
 }
