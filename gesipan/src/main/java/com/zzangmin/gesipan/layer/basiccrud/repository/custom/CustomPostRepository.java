ef34863ce6021df1c284ad2351a9e86a4d7f069e
leezzangmin <ckdals12345678@gmail.com>
2022-10-31 21:53:07 +0900

    refactor: 파일명 변경 및 인터페이스 상속관계 제거, JPAQueryFactory 의존성 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/layer/basiccrud/repository/custom/CustomPostRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/layer/basiccrud/repository/custom/CustomPostRepository.java
@@ -1,9 +1,49 @@
 package com.zzangmin.gesipan.layer.basiccrud.repository.custom;
 
+
+import static com.zzangmin.gesipan.layer.basiccrud.entity.QPost.post;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.zzangmin.gesipan.layer.basiccrud.dto.post.PostSearchRequest;
 import com.zzangmin.gesipan.layer.basiccrud.entity.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-public interface CustomPostRepository {
-    List<Post> searchPostsWithUser(PostSearchRequest postSearchRequest);
+
+@RequiredArgsConstructor
+@Repository
+public class CustomPostRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public List<Post> searchPostsWithUser(PostSearchRequest postSearchRequest) {
+        JPQLQuery query = jpaQueryFactory.from(post)
+            .select(post)
+            .join(post.user).fetchJoin()
+            .where(post.postCategory.postCategoryId.eq(postSearchRequest.getPostCategoryId()),
+                eqUserNickname(postSearchRequest.getUserNickname()),
+                betweenDate(postSearchRequest.getStartAt(), postSearchRequest.getEndAt()));
+
+        return query.fetch();
+    }
+
+    private BooleanExpression eqUserNickname(String userNickname) {
+        if (userNickname != null) {
+            return post.user.userNickname.eq(userNickname);
+        }
+        return null;
+    }
+
+    private BooleanExpression betweenDate(LocalDateTime startAt, LocalDateTime endAt) {
+        if (startAt != null) {
+            return post.createdAt.between(startAt, endAt);
+        }
+        return null;
+    }
+
 }
