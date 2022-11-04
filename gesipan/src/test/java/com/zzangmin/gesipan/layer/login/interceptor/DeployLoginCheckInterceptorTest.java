5c0b75c213f04caa2940e673f392a3be24e92220
leezzangmin <ckdals12345678@gmail.com>
2022-11-04 22:44:42 +0900

    test: 로그인 인터셉터 정상토큰 흐름 통과 검증 테스트 추가

--- gesipan/src/test/java/com/zzangmin/gesipan/layer/login/interceptor/DeployLoginCheckInterceptorTest.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/login/interceptor/DeployLoginCheckInterceptorTest.java
@@ -12,7 +12,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 
 class DeployLoginCheckInterceptorTest {
 
-    JwtProvider jwtProvider = new JwtProvider();
+    JwtProvider jwtProvider = new JwtProvider("fake_secret");
     DeployLoginCheckInterceptor deployLoginCheckInterceptor = new DeployLoginCheckInterceptor(jwtProvider);
 
     @DisplayName("변조된 토큰을 HttpRequest에 쿠키로 담아 요청하면 에러를 발생시켜야 한다.")
@@ -21,11 +21,26 @@ class DeployLoginCheckInterceptorTest {
         //given
         MockHttpServletRequest request = new MockHttpServletRequest();
         MockHttpServletResponse response = new MockHttpServletResponse();
-        Cookie cookie = new Cookie("X-AUTH", "가짜토큰, 인증실패해야함");
+        Cookie cookie = new Cookie("X-AUTH-TOKEN", "가짜토큰, 인증실패해야함");
         request.setCookies(cookie);
         //when then
-        Assertions.assertThatThrownBy(() -> deployLoginCheckInterceptor.preHandle(request,response,null));
+        Assertions.assertThatThrownBy(() -> deployLoginCheckInterceptor.preHandle(request, response, null));
     }
 
+    @DisplayName("정상 토큰을 HttpRequest에 쿠키로 담아 요청하면 true를 반환해야 한다")
+    @Test
+    void 정상토큰통과테스트() {
+        //given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        String realToken = jwtProvider.createToken(1L);
+        System.out.println("realToken = " + realToken);
+        Cookie cookie = new Cookie("X-AUTH-TOKEN", realToken);
+        request.setCookies(cookie);
+        //when
+        boolean returnValue = deployLoginCheckInterceptor.preHandle(request, response, null);
+        //then
+        Assertions.assertThat(returnValue).isTrue();
+    }
 
 }
