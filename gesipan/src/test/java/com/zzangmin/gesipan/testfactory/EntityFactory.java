926ec6e22d131b1f4e8005846e50c89117e8f74d
leezzangmin <ckdals12345678@gmail.com>
2022-11-09 01:18:34 +0900

    test: 엔티티 팩토리 메서드 추가

--- gesipan/src/test/java/com/zzangmin/gesipan/testfactory/EntityFactory.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/testfactory/EntityFactory.java
@@ -16,7 +16,7 @@ public class EntityFactory {
         throw new AssertionError();
     }
 
-    public static Comment makeRandomCommentObject() {
+    public static Comment generateRandomCommentObject() {
         Users user = generateRandomUsersObject();
         Post post = generateRandomPostObject();
 
@@ -24,7 +24,39 @@ public class EntityFactory {
             .commentContent(UUID.randomUUID().toString())
             .post(post)
             .user(user)
-            .baseTime(generateNOWBaseTime())
+            .baseTime(generateRandomBaseTime())
+            .build();
+    }
+
+    public static Comment generateCommentObject() {
+        Users user = generateRandomUsersObject();
+        Post post = generateRandomPostObject();
+
+        return Comment.builder()
+            .commentContent(UUID.randomUUID().toString())
+            .post(post)
+            .user(user)
+            .baseTime(generateFixedBaseTime())
+            .build();
+    }
+
+    public static Comment generateCommentObject(Post post) {
+        Users user = generateRandomUsersObject();
+
+        return Comment.builder()
+            .commentContent(UUID.randomUUID().toString())
+            .post(post)
+            .user(user)
+            .baseTime(generateFixedBaseTime())
+            .build();
+    }
+
+    public static Comment generateCommentObject(Post post, Users user) {
+        return Comment.builder()
+            .commentContent(UUID.randomUUID().toString())
+            .post(post)
+            .user(user)
+            .baseTime(generateFixedBaseTime())
             .build();
     }
 
@@ -44,27 +76,45 @@ public class EntityFactory {
             .user(user)
             .postCategory(postCategory)
             .hitCount(0L)
-            .baseTime(generateNOWBaseTime())
+            .baseTime(generateRandomBaseTime())
             .build();
     }
+
+    public static Post generateRandomPostObject(Users user) {
+        PostCategory postCategory = generatePostCategoryObject(Categories.FREE);
+
+        return Post.builder()
+            .postSubject(generateRandomUUIDString())
+            .postContent(generateRandomUUIDString())
+            .user(user)
+            .postCategory(postCategory)
+            .hitCount(0L)
+            .baseTime(generateRandomBaseTime())
+            .build();
+    }
+
     public static Users generateRandomUsersObject() {
         return Users.builder()
             .userEmail(generateRandomUUIDString())
-            .userName(generateRandomUUIDString())
+            .userName(generateRandomUUIDString().substring(0, 5))
             .userNickname(generateRandomUUIDString())
             .userRole(UserRole.NORMAL)
-            .baseTime(generateNOWBaseTime())
+            .baseTime(generateRandomBaseTime())
             .build();
     }
 
-    public static BaseTime generateNOWBaseTime() {
+    public static BaseTime generateRandomBaseTime() {
         LocalDateTime now = LocalDateTime.now();
         return new BaseTime(now, now);
     }
+
+    public static BaseTime generateFixedBaseTime() {
+        LocalDateTime fixed = LocalDateTime.of(2022,01, 01, 01, 01, 01);
+        return new BaseTime(fixed, fixed);
+    }
+
     private static String generateRandomUUIDString() {
         return UUID.randomUUID().toString();
     }
-
-
 
 }
