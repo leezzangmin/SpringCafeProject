a5e282baa2e57712a822b12af1021aad5d010d5f
leezzangmin <ckdals12345678@gmail.com>
2022-11-10 15:21:40 +0900

    test: 올바르지 않은 postCategoryId로 게시글 저장 요청을 보내면 오류가 발생해야 한다

--- gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/service/PostServiceTest.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/service/PostServiceTest.java
@@ -144,6 +144,23 @@ class PostServiceTest {
         Assertions.assertThatThrownBy(() -> postService.save(invalidUserId, postSaveRequest));
     }
 
+    @DisplayName("올바르지 않은 postCategoryId로 게시글 저장 요청을 보내면 오류가 발생해야 한다.")
+    @Test
+    void save_invalidPostCategoryId() {
+        //given
+        Users user = EntityFactory.generateRandomUsersObject();
+        Post post = EntityFactory.generateRandomPostObject(user);
+        usersRepository.save(user);
+        postCategoryRepository.save(post.getPostCategory());
+        postRepository.save(post);
+        Long invalidPostCategoryId = 13849137489999999L;
+        PostSaveRequest postSaveRequest = new PostSaveRequest("asdf", "dddd", invalidPostCategoryId, LocalDateTime.now(), null);
+        //when
+
+        //then
+        Assertions.assertThatThrownBy(() -> postService.save(user.getUserId(), postSaveRequest));
+    }
+
     @Test
     @DisplayName("임시 게시물을 불러와서 저장하면 임시 게시물은 삭제되어야 한다.")
     void save2() {
