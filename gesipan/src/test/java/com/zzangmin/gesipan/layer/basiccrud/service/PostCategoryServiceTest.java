eecd5ab67f947dab813587be237d5ed3967c9d8b
leezzangmin <ckdals12345678@gmail.com>
2022-11-12 23:06:10 +0900

    test: 정상 postCategoryId로 조회를 요청하면 PostCategory를 반환해야한다

--- gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/service/PostCategoryServiceTest.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/service/PostCategoryServiceTest.java
@@ -2,6 +2,10 @@ package com.zzangmin.gesipan.layer.basiccrud.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.zzangmin.gesipan.layer.basiccrud.entity.Categories;
+import com.zzangmin.gesipan.layer.basiccrud.entity.PostCategory;
+import com.zzangmin.gesipan.layer.basiccrud.repository.PostCategoryRepository;
+import com.zzangmin.gesipan.testfactory.EntityFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PostCategoryServiceTest {
 
     @Autowired PostCategoryService postCategoryService;
+    @Autowired PostCategoryRepository postCategoryRepository;
 
     @DisplayName("존재하지 않는 postCategoryId로 조회를 요청하면 오류가 발생해야 한다.")
     @Test
@@ -21,5 +26,19 @@ class PostCategoryServiceTest {
         //when
         //then
         Assertions.assertThatThrownBy(() -> postCategoryService.findOne(invalidPostCategoryId));
+    }
+
+    @DisplayName("정상 postCategoryId로 조회를 요청하면 PostCategory를 반환해야한다.")
+    @Test
+    void findOne() {
+        //given
+        PostCategory postCategory = EntityFactory.generatePostCategoryObject(Categories.FREE);
+        postCategoryRepository.save(postCategory);
+        Long postCategoryId = postCategory.getPostCategoryId();
+        //when
+        PostCategory findPostCategory = postCategoryService.findOne(postCategoryId);
+        //then
+        Assertions.assertThat(postCategory.getPostCategoryId()).isEqualTo(findPostCategory.getPostCategoryId());
+        Assertions.assertThat(postCategory.getCategoryName()).isEqualTo(findPostCategory.getCategoryName());
     }
 }
