0fd5bb75937139cbdfc314dfd8714a1c35f5cfb1
leezzangmin <ckdals12345678@gmail.com>
2022-11-12 23:01:30 +0900

    test: PostCategoryServiceTest 생성

---
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/service/PostCategoryServiceTest.java
@@ -0,0 +1,17 @@
+package com.zzangmin.gesipan.layer.basiccrud.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCategoryServiceTest {
+
+
+
+    @Test
+    void findOne() {
+    }
+}
