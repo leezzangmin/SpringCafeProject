fd9437f92fd93876096040252cd4425ac0e06c24
leezzangmin <ckdals12345678@gmail.com>
2022-11-14 15:25:56 +0900

    test: 테스트 리팩토링 및 이상한 테스트 삭제

--- gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/repository/PostRepositoryTest.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/repository/PostRepositoryTest.java
@@ -2,22 +2,16 @@ package com.zzangmin.gesipan.layer.basiccrud.repository;
 
 import com.zzangmin.gesipan.layer.basiccrud.entity.Post;
 import com.zzangmin.gesipan.layer.basiccrud.entity.PostCategory;
-import com.zzangmin.gesipan.layer.embeddable.BaseTime;
-import com.zzangmin.gesipan.layer.login.entity.UserRole;
 import com.zzangmin.gesipan.layer.login.entity.Users;
 import com.zzangmin.gesipan.layer.login.repository.UsersRepository;
-import com.zzangmin.gesipan.layer.basiccrud.entity.Categories;
+import com.zzangmin.gesipan.testfactory.EntityFactory;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @DataJpaTest
@@ -30,65 +24,28 @@ class PostRepositoryTest {
     @Autowired
     UsersRepository usersRepository;
 
-    PostCategory postCategory;
-    Users user;
-    Post post;
 
-    @BeforeEach
-    void setUp() {
-        postCategory = PostCategory.builder()
-                .categoryName(Categories.FREE)
-                .build();
-        user = Users.builder()
-                .userEmail("ckdals1234@naver.com")
-                .userName("이창민")
-                .userNickname("zzangmin")
-                .userRole(UserRole.NORMAL)
-                .baseTime(new BaseTime(LocalDateTime.of(2022,2,2,2,2), LocalDateTime.of(2022,2,2,2,2)))
-                .build();
-
-        post = Post.builder()
-                .postSubject("test제목")
-                .postContent("test내용")
-                .user(user)
-                .postCategory(postCategory)
-                .baseTime(new BaseTime(LocalDateTime.of(2022,2,2,2,2), LocalDateTime.of(2022,2,2,2,2)))
-                .hitCount(0L)
-                .build();
-        usersRepository.save(user).getUserId();
-        postCategoryRepository.save(postCategory);
-        postRepository.save(post).getPostId();
-    }
-
+    @DisplayName("postId로 게시글+유저 조회를 수행해야 한다")
     @Test
     void findByIdWithUser() {
         //given
+        Users user = EntityFactory.generateRandomUsersObject();
+        Post post = EntityFactory.generateRandomPostObject(user);
+        PostCategory postCategory = post.getPostCategory();
+
+        usersRepository.save(user);
+        postCategoryRepository.save(postCategory);
+        postRepository.save(post);
+
         Long userId = user.getUserId();
         Long postCategoryId = postCategory.getPostCategoryId();
         Long postId = post.getPostId();
         //when
-        Post post = postRepository.findByIdWithUser(postId).get();
+        Post findPost = postRepository.findByIdWithUser(postId).get();
         //then
-        org.assertj.core.api.Assertions.assertThat(post.getPostId()).isEqualTo(postId);
-        org.assertj.core.api.Assertions.assertThat(post.getUser().getUserId()).isEqualTo(userId);
+        Assertions.assertThat(findPost.getPostId()).isEqualTo(postId);
+        Assertions.assertThat(findPost.getUser().getUserId()).isEqualTo(userId);
 
     }
 
-    @Test
-    void findPageByCategoryId() {
-        //given
-        Long userId = user.getUserId();
-        Long postCategoryId = postCategory.getPostCategoryId();
-        Long postId = post.getPostId();
-        Pageable pageable = PageRequest.of(0,10);
-
-        //when
-        List<Long> postIds = postRepository.findPaginationPostIdsByCategoryId(postCategoryId, pageable);
-        List<Post> posts = postRepository.paginationByPostIds(postIds);
-        List<Long> nonCategorizedPosts = postRepository.findPaginationPostIdsByCategoryId(99999L, pageable);
-
-        //then
-        org.assertj.core.api.Assertions.assertThat(posts.size()).isLessThan(10).isGreaterThan(0);
-        Assertions.assertThat(nonCategorizedPosts.size()).isEqualTo(0);
-    }
 }
