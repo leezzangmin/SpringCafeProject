305a44e6860ca54f65bde1702bcad160cafe72fd
leezzangmin <ckdals12345678@gmail.com>
2022-11-15 15:15:06 +0900

    feat: 유저 개인 게시물 조회 쿼리 반환타입 DTO로 변경, 쿼리수정

--- gesipan/src/main/java/com/zzangmin/gesipan/layer/basiccrud/repository/PostRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/layer/basiccrud/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.zzangmin.gesipan.layer.basiccrud.repository;
 
 import com.zzangmin.gesipan.layer.basiccrud.dto.post.PostSimpleQueryDTO;
+import com.zzangmin.gesipan.layer.basiccrud.dto.post.SimpleUsersPostQueryDTO;
 import com.zzangmin.gesipan.layer.basiccrud.entity.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,8 +23,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "from Post p inner join p.user u " +
             "where p.postId in :postIds and u.userId=p.user.userId")
     List<PostSimpleQueryDTO> paginationByPostIds(@Param("postIds") List<Long> postIds);
-    @Query("select p from Post p where p.user.userId=:userId")
-    List<Post> findByUserId(@Param("userId") Long userId);
+    @Query("select new com.zzangmin.gesipan.layer.basiccrud.dto.post.SimpleUsersPostQueryDTO(p.postId, p.postSubject, p.baseTime.createdAt, p.hitCount) " +
+            "from Post p " +
+            "where p.user.userId=:userId")
+    List<SimpleUsersPostQueryDTO> findByUserId(@Param("userId") Long userId);
 
     @Modifying
     @Query("update Post p set p.hitCount = p.hitCount + :hitCount where p.postId =:postId")
