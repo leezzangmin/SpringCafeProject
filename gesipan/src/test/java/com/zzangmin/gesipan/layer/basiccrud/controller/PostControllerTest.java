96928848567af0f7e98c8ec8cbd8d0c0a7ee0aa7
leezzangmin <ckdals12345678@gmail.com>
2022-11-16 16:15:22 +0900

    test: PostControllerTest.java 생성

---
+++ gesipan/src/test/java/com/zzangmin/gesipan/layer/basiccrud/controller/PostControllerTest.java
@@ -0,0 +1,11 @@
+package com.zzangmin.gesipan.layer.basiccrud.controller;
+
+import com.zzangmin.gesipan.layer.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest extends BaseIntegrationTest {
+
+
+}
