ab0d020878f46d513de4a88bda42f2b4f9be2754
leezzangmin <ckdals12345678@gmail.com>
2022-11-17 17:38:16 +0900

    feat: 쿼리 반환타입 DTO로 변경

--- gesipan/src/main/java/com/zzangmin/gesipan/component/basiccrud/repository/CommentRepository.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/component/basiccrud/repository/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.zzangmin.gesipan.component.basiccrud.repository;
 
+import com.zzangmin.gesipan.component.basiccrud.dto.comment.CommentResponse;
 import com.zzangmin.gesipan.component.basiccrud.entity.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -28,6 +29,11 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select c.commentId from Comment c where c.post.postId=:postId")
     List<Long> findCommentIdsByPaginationByPostId(@Param("postId") Long postId, Pageable pageable);
 
-    @Query("select c from Comment c join fetch c.user where c.commentId in :commentIds")
-    List<Comment> findByIdsWithUsers(@Param("commentIds") List<Long> commentIds);
+    //@Query("select c from Comment c join fetch c.user where c.commentId in :commentIds")
+    @Query("select new com.zzangmin.gesipan.component.basiccrud.dto.comment.CommentResponse(c.commentId, c.commentContent, c.baseTime.createdAt, c.baseTime.updatedAt, u.userId, u.userNickname) "
+        + "from Comment c "
+        + "inner join c.user u "
+        + "where c.commentId in :commentIds and "
+        + "c.user.userId = u.userId")
+    List<CommentResponse> findByIdsWithUsers(@Param("commentIds") List<Long> commentIds);
 }
