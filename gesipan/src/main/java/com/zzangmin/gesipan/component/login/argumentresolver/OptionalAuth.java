aaf10afc91495c720acd2f6d70114982a5be042b
leezzangmin <ckdals12345678@gmail.com>
2022-11-19 15:09:29 +0900

    feat: OptionalAuth 추가.

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/component/login/argumentresolver/OptionalAuth.java
@@ -0,0 +1,11 @@
+package com.zzangmin.gesipan.component.login.argumentresolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OptionalAuth {
+}
