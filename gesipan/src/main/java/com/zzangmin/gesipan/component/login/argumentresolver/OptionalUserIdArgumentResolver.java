389e9041491cf7654f305f27f03453e67d09924c
leezzangmin <ckdals12345678@gmail.com>
2022-11-19 15:09:42 +0900

    feat: OptionalAuth argument Resolver 추가.

---
+++ gesipan/src/main/java/com/zzangmin/gesipan/component/login/argumentresolver/OptionalUserIdArgumentResolver.java
@@ -0,0 +1,39 @@
+package com.zzangmin.gesipan.component.login.argumentresolver;
+
+
+import com.zzangmin.gesipan.component.login.service.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class OptionalUserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasOptionalAuthAnnotation = parameter.hasParameterAnnotation(OptionalAuth.class);
+        boolean isOptionalType = Optional.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasOptionalAuthAnnotation && isOptionalType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) webRequest.getNativeRequest();
+        Optional<String> jwt = jwtProvider.resolveToken(httpServletRequest);
+        if (jwt.isEmpty()) {
+            return Optional.empty();
+        }
+        return jwtProvider.getUserIdFromToken(jwt.get());
+    }
+}
