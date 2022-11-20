44c325a599916aa37f872cbfa947c62ac87b5ab9
leezzangmin <ckdals12345678@gmail.com>
2022-11-20 22:02:46 +0900

    style: 불필요 주석 제거

--- gesipan/src/main/java/com/zzangmin/gesipan/component/login/controller/LoginController.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/component/login/controller/LoginController.java
@@ -16,14 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-/** TODO: 1. code 요청하는 URL 돌려주는 api
- *        2. callback+code로 요청하면
- *          2 - 1 : access_token 발급 및 어딘가에 저장 (post요청)
- *          2 - 2 : access_token 으로 깃허브에서 resource 받아옴 (get요청) // 이름, 이메일, 닉네임
- *          2 - 3 : 받아온 resource(user)가 서버(DB,메모리)에 있거나 없으면 upsert(update+insert)
- *          2 - 4 : 받아온 정보로 암호화된 (user,expired) 정보를 넣은 JWT를 cookie에 담아 반환
- *
- */
 @Slf4j
 @AllArgsConstructor
 @RestController
@@ -32,14 +24,12 @@ public class LoginController {
     private final GithubOauthService githubOauthService;
     private final JwtProvider jwtProvider;
 
-    // https://github.com/login/oauth/authorize?client_id=2e32ee13d9ee74d112ec&scope=user
     @GetMapping("/oauth/github-url")
     public ResponseEntity<String> githubOauthURL() {
         return ResponseEntity.ok(githubOauthURL);
     }
 
 
-    // accesstoken을 왜 저장해야 하는거지
     @GetMapping("/login")
     public void callbackAndLoginProcess(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         GithubToken githubAccessToken = githubOauthService.getAccessToken(code);
