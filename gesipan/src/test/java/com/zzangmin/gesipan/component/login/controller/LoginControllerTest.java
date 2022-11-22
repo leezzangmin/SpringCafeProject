909b175a8ebd7e8a44ad17bed99d3d0a9f71ddd4
leezzangmin <ckdals12345678@gmail.com>
2022-11-22 15:03:50 +0900

    test: 로그인을 수행하면 유저가 저장되어있어야 한다

--- gesipan/src/test/java/com/zzangmin/gesipan/component/login/controller/LoginControllerTest.java
+++ gesipan/src/test/java/com/zzangmin/gesipan/component/login/controller/LoginControllerTest.java
@@ -6,12 +6,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.zzangmin.gesipan.component.BaseIntegrationTest;
+import com.zzangmin.gesipan.component.login.entity.Users;
+import com.zzangmin.gesipan.component.login.repository.UsersRepository;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.ResultActions;
 
 class LoginControllerTest extends BaseIntegrationTest {
 
+
+    @Autowired
+    UsersRepository usersRepository;
 
     @DisplayName("로그인을 수행하면 쿠키가 반환되어야 한다.")
     @Test
@@ -24,5 +32,18 @@ class LoginControllerTest extends BaseIntegrationTest {
         result
             .andExpect(status().isOk())
             .andExpect(cookie().exists("X-AUTH-TOKEN"));
+    }
+
+    @DisplayName("로그인을 수행하면 유저가 저장되어있어야 한다.")
+    @Test
+    void callbackAndLoginProcess_userSave() throws Exception {
+        //given
+        String code = "testCode";
+        //when
+        mvc.perform(get("/login").param("code", code));
+        //then
+        List<Users> all = usersRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(1);
+
     }
 }
