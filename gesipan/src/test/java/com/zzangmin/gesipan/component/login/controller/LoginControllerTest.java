6104756b66136d50832b69670d10edd78b274bd7
leezzangmin <ckdals12345678@gmail.com>
2022-11-22 14:05:50 +0900

    test: LoginControllerTest 생성

---
+++ gesipan/src/test/java/com/zzangmin/gesipan/component/login/controller/LoginControllerTest.java
@@ -0,0 +1,16 @@
+package com.zzangmin.gesipan.component.login.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.zzangmin.gesipan.component.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest extends BaseIntegrationTest {
+
+
+
+
+    @Test
+    void callbackAndLoginProcess() {
+    }
+}
