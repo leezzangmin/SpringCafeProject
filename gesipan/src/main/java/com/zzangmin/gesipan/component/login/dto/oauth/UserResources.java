cc6716e23caa58836e104df6bfac618ee597f470
leezzangmin <ckdals12345678@gmail.com>
2022-11-22 14:11:27 +0900

    feat: toEntity 메서드 추가

--- gesipan/src/main/java/com/zzangmin/gesipan/component/login/dto/oauth/UserResources.java
+++ gesipan/src/main/java/com/zzangmin/gesipan/component/login/dto/oauth/UserResources.java
@@ -2,6 +2,10 @@ package com.zzangmin.gesipan.component.login.dto.oauth;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zzangmin.gesipan.component.embeddable.BaseTime;
+import com.zzangmin.gesipan.component.login.entity.UserRole;
+import com.zzangmin.gesipan.component.login.entity.Users;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -18,4 +22,14 @@ public class UserResources {
     @JsonProperty("login")
     private final String userNickname;
 
+    public Users toEntity() {
+        LocalDateTime now = LocalDateTime.now();
+        return Users.builder()
+            .userEmail(this.getUserEmail())
+            .userName(this.getUserName())
+            .userNickname(this.getUserNickname())
+            .userRole(UserRole.NORMAL)
+            .baseTime(new BaseTime(now, now))
+            .build();
+    }
 }
